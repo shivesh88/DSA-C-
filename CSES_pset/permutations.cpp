9d0523fa793785a15046bcd565d0ed354711d1ac
shivesh <cpdwivedi916@gmail.com>
2023-10-21 01:00:30 +0530

    cses4

---
+++ CSES_pset/permutations.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+problem:-https://cses.fi/problemset/task/1070/
+
+idea:-
+every consecutive odd or even number diffrence is 2
+*/
+int main(){
+    long long int n;
+    cin>>n;
+    if(n==1){cout<<1;
+    return 0;}
+    if(n<4){
+        cout<<"NO SOLUTION";
+        return 0;
+    }
+    for(int i=2;i<=n;i+=2){
+        cout<<i<<" ";
+    }
+    for(int i=1;i<=n;i+=2){
+        cout<<i<<" ";
+    }
+    return 0;
+}
