88cc153390c76a1bb1dddb9ea9235580a7236311
shivesh88 <76835391+shivesh88@users.noreply.github.com>
2024-01-24 12:44:48 +0000

    vc

--- kuchbhi/hey.cpp
+++ kuchbhi/hey.cpp
@@ -1,51 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int distance(vector<vector<int>> &a, vector<int> &b, vector<int> &c, int n,int m)
-{
-    if(b==c){
-        return 0;
-    }
-    if(c[0]<0||c[0]>=n||c[1]>=m||c[1]<0){
-        return -1;
-    }
-    if(a[b[0]][b[1]]==1||a[c[0]][c[1]]==1){
-        return -1;
-    }
-    int shortest=-1;
+
+int distance(vector<vector<int>> &a, const vector<int> &b,
+             vector<int> &c, int n, int m,vector<vector<bool>>&visited) {
     
-
-
-
-    c={c[0]-1,c[1]};
-    int d = distance(a,b,c,n,m);
-    if(d!=-1||d<shortest)
-    shortest=d;
-
-
-    c={c[0]+1,c[1]};
-    d=distance(a,b,c,n,m);
-    if(d!=-1&&(shortest==-1||d<shortest))
-    shortest=d;
-
-
-    c={c[0],c[1]-1};
-    d=distance(a,b,c,n,m);
-    if(d!=-1&&(shortest==-1||d<shortest))
-    shortest=d;
-
-
-    c={c[0],c[1]+1};
-    d=distance(a,b,c,n,m);
-    if(d!=-1&&(shortest==-1||d<shortest))
-    shortest=d;
-    return shortest;
 }
+
 int main() {
-    vector<vector<int> > A={{1,1,0,1},{0,0,0,1},{1,0,0,1},{0,0,1,0}};
-    vector<int> B={1,1};
-    vector<int> C={2,1};
-    if(A[C[0]][C[1]]==0&&A[B[0]][B[1]]==0){
-        cout<< distance(A,B,C,(int)A.size(),(int)A[0].size());
-    }
-    else cout<< -1;
+    vector<vector<int>> A = {{1, 1, 0, 1}, {0, 0, 0, 1}, {1, 0, 0, 1}, {0, 0, 1, 0}};
+    vector<int> B = {1, 1};
+    vector<int> C = {2, 1};
+    int n = A.size();
+    int m = A[0].size();
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+    cout << distance(A, B, C, n, m, visited);
+    return 0;
 }
